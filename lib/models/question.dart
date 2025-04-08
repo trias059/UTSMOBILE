ef32afacf28a6f357ed52ddbf013bb682e099a12
trias059 <148695060+trias059@users.noreply.github.com>
2025-04-08 22:47:57 +0700

    Update question.dart

--- lib/models/question.dart
+++ lib/models/question.dart
@@ -1,7 +1,7 @@
-class Question {
-  String questionText;
-  List<String> options;
-  int correctAnswerIndex;
+class Question { // class soal
+  String questionText; // question berbentuk string
+  List<String> options;// list string yang memungkin menyimpan opsi lebih dari 1
+  int correctAnswerIndex;// penentuan jawaban yang benar
 
   Question({required this.questionText, required this.options, required this.correctAnswerIndex});
 }
